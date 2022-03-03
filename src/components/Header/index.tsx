4fe9d1df3a5844f53a912977ff831f32b67f21cd
Luiz Eduardo Matos <luizmatosedu@gmail.com>
2022-03-02 21:46:52 -0300

    Commit inicial Criação de Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import logo from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt=""/>
+        <button type="button">
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
+}
+
+export default Header
