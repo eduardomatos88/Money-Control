c08b1e4925791cb5553cd7f2642f6f83762cb579
Luiz Eduardo Barros Matos <f612247@corp.caixa.gov.br>
2022-03-25 10:14:45 -0300

    Adicionado modal do react-modal para novas transações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
 import logo from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logo} alt=""/>
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
+
       </Content>
     </Container>
   )
